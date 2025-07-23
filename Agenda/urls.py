101357ca01b828fe18429cf1709d621420e414d7
Alexi Ramirez <rf0196022023@unab.edu.sv>
2025-07-23 14:11:10 -0600

    ruta raiz

--- Agenda/urls.py
+++ Agenda/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', include('Contact.urls')),
     path('todo/', include('Todo.urls')),
-    path('index/', views.index, name="index"),
+    path('', views.index, name="index"),
     path('signup/', views.signup, name="signup"),
     path('logout/', views.signout, name="logout"),
     path('signin/', views.signin, name="signin"),
